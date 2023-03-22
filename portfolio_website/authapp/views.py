abd88c1d004a850aa122c7d7c7cc15cf4f8bdfea
yogendra dhami <yogendradhami6@gmail.com>
2023-03-23 02:02:27 +0545

    register form has been created

--- portfolio_website/authapp/views.py
+++ portfolio_website/authapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def SignUp(request):
+    return render(request, 'authentication/signup.html')
+
+def Login(request):
+    return render(request, 'authentication/login.html')
+
+def Logout(request):
+    return render(request, 'authentication/login.html')
--- portfolio_website/portfolio_website/urls.py
+++ portfolio_website/portfolio_website/urls.py
@@ -19,6 +19,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', include('blog.urls')),
-    # path('', include('authapp.urls')),
+    path('authentication/', include('authapp.urls')),
     path('', include('portfolio.urls')),
 ]

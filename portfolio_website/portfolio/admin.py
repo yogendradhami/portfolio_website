d8d4c7c5d222a6090791de3fe3389e5b5781cdf1
yogendra dhami <yogendradhami6@gmail.com>
2023-03-23 00:41:46 +0545

    portfolio website initialized

---
+++ portfolio_website/portfolio/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+# from.models import
+# Register your models here.
+
+
+
+# to rename the admin panel records
+
+admin.site.site_header= "Portfolio"
+admin.site.site_title= 'Portfolio'
+admin.site.index_title= 'Admin_panel'
